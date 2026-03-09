7da6179fe3e1af53fa6f8b0f46c0869bd411d13d
N.Balasubramanya <nbalun1224@gmail.com>
2026-03-09 18:04:51 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://document-me-net.vercel.app/',
+  apiBaseUrl: 'https://document-me-net.vercel.app',
   hocuspocusUrl: 'https://document-me-hocuspocus.onrender.com'
 };
