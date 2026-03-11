6097fffc0e9525213149b9d458d4efa39d73dc45
N.Balasubramanya <nbalun1224@gmail.com>
2026-03-11 12:44:29 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://document-me-net-production.up.railway.app',
+  apiBaseUrl: 'https://document-me-net-production.up.railway.app/api',
   hocuspocusUrl: 'https://document-me-hocuspocus.onrender.com'
 };
